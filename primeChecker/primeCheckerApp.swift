2675c6c4a6b22e6bc8f49f96269eaee9a4fc7b52
vaibhav-hackerrank <vaibhav.gupta@hackerrank.com>
2025-01-08 13:27:58 +0530

    Initial Commit

---
+++ primeChecker/primeCheckerApp.swift
@@ -0,0 +1,17 @@
+//
+//  primeCheckerApp.swift
+//  primeChecker
+//
+//  Created by Vaibhav Gupta on 08/01/25.
+//
+
+import SwiftUI
+
+@main
+struct primeCheckerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ primeCheckerTests/primeCheckerTests.swift
@@ -0,0 +1,17 @@
+//
+//  primeCheckerTests.swift
+//  primeCheckerTests
+//
+//  Created by Vaibhav Gupta on 08/01/25.
+//
+
+import Testing
+@testable import primeChecker
+
+struct primeCheckerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
